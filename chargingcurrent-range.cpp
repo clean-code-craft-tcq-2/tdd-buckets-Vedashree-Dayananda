a2cc7fb9567fa185b1f95773d7a1a339929cbff2
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-03-09 23:25:32 +0530

    Update chargingcurrent-range.cpp

--- chargingcurrent-range.cpp
+++ chargingcurrent-range.cpp
@@ -1,12 +1,14 @@
 #include "chargingcurrent-range.h"
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 std::vector<Range> computeCurrentRanges(std::vector<int> readingsList)
 {
 	std::vector<Range> listofRanges = {};
 	Range currentRange = {};
-  
+	
+	sort(readingsList.begin(), readingsList.end());
 	currentRange.lowerLimit = readingsList[0];
 	currentRange.upperLimit = readingsList[0];
 	currentRange.numOfReadingsInRange = 0;
@@ -16,6 +18,13 @@ std::vector<Range> computeCurrentRanges(std::vector<int> readingsList)
 		{
 			currentRange.upperLimit = *itr;
 			currentRange.numOfReadingsInRange++;
+		}
+		else
+		{
+			listofRanges.push_back(currentRange);
+			currentRange.lowerLimit = *itr;
+			currentRange.upperLimit = *itr;
+			currentRange.numOfReadingsInRange = 1;
 		}
 	}
 	listofRanges.push_back(currentRange);
