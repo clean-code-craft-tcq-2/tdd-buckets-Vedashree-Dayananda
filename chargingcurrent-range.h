7202cb992263a21db502c75456627e4113f13f11
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-03-09 22:26:55 +0530

    Update and rename chargingcurrent.h to chargingcurrent-range.h

---
+++ chargingcurrent-range.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+
+struct Range
+{
+	int lowerLimit;
+	int upperLimit;
+	int numOfReadingsInRange;
+};
+
+std::vector<Range> computeCurrentRanges(std::vector<int> readingsList);
