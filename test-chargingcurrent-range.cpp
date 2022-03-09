40dd123dfb3ac8191dc6fd1c67fa9d27edec2258
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-03-09 22:27:42 +0530

    Update test-chargingcurrent-range.cpp

--- test-chargingcurrent-range.cpp
+++ test-chargingcurrent-range.cpp
@@ -1,1 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test/catch.hpp"
+#include "chargingcurrent-range.h"
+
+TEST_CASE("detect the ranges in the given input list") {
+	std::vector<Range> listofRanges = {};
+	std::vector<int> inputList = { 4, 5 };
+	listofRanges = computeCurrentRanges(inputList);
+	REQUIRE(listofRanges.size() == 1);
+}
