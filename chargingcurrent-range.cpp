57bd57b21f2341892e6db382954a6b2e3203416b
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-03-09 22:41:22 +0530

    Update chargingcurrent-range.cpp

--- chargingcurrent-range.cpp
+++ chargingcurrent-range.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 std::vector<Range> computeCurrentRanges(std::vector<int> readingsList)
 {
-  return {};
+	  std::vector<Range> listofRanges = {};
+	  Range currentRange = {};
+  
+	  currentRange.lowerLimit = readingsList[0];
+	  currentRange.upperLimit = readingsList[0];
+	  currentRange.numOfReadingsInRange = 0;
+	  for (std::vector<int>::iterator itr = readingsList.begin(); itr != readingsList.end(); itr++)
+		{
+		  	if (*itr - currentRange.upperLimit <= 1)
+			  {
+				    currentRange.upperLimit = *itr;
+				    currentRange.numOfReadingsInRange++;
+			  }
+			  else
+			  {
+				    listofRanges.push_back(currentRange);
+				    currentRange.lowerLimit = *itr;
+				    currentRange.lowerLimit = *itr;
+				    currentRange.numOfReadingsInRange = 1;
+		  	}
+    }
+  return listofRanges;
 }
