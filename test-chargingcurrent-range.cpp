4988ab2059c156d5ab9556faf200ef99ab8b30d5
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-03-09 22:58:39 +0530

    Update test-chargingcurrent-range.cpp

--- test-chargingcurrent-range.cpp
+++ test-chargingcurrent-range.cpp
@@ -3,10 +3,12 @@
 #include "test/catch.hpp"
 #include "chargingcurrent-range.h"
 
-TEST_CASE("detect the ranges in the given input list") {
+TEST_CASE("detect the ranges in the given input list with 2 elements") {
 	std::vector<Range> listofRanges = {};
 	std::vector<int> inputList = { 4, 5 };
 	listofRanges = computeCurrentRanges(inputList);
 	REQUIRE(listofRanges.size() == 1);
+	REQUIRE(listofRanges[0].lowerLimit == 4);
+	REQUIRE(listofRanges[0].upperLimit == 5);
 	REQUIRE(listofRanges[0].numOfReadingsInRange == 2);
 }
