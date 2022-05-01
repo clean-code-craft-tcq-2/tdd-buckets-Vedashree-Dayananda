e1b968851a44fba9f748b61970dd437e3e6ad33d
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-05-01 21:04:22 +0530

    Update chargingcurrent-range.cpp

--- chargingcurrent-range.cpp
+++ chargingcurrent-range.cpp
@@ -3,14 +3,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void displayRanges(std::vector<Range> listofRanges)
+std::map < int, int> MaximumLimitsMap = {
+	{ 12, 4094 },
+	{ 10, 1022 }
+};
+
+using funcptr = std::vector<double>(*)(std::vector<double>);
+
+std::map < int, funcptr > ConverterFunctionMap = {
+	{ 12, converterfor12BitSensor },
+	{ 10, converterfor10BitSensor }
+};
+
+void printOnConsole(std::string outputText)
 {
-	cout << "Range, Reading" << endl;
-	for (int i = 0; i < listofRanges.size(); i++)
-		cout << listofRanges[i].lowerLimit << "-" << listofRanges[i].upperLimit << ", " << listofRanges[i].numOfReadingsInRange << endl;
+	cout << outputText;
 }
 
-std::vector<Range> findRanges(std::vector<int> readingsList)
+std::string formatOutputToCsv(std::vector<Range> listofRanges)
+{
+	std::string outputText = "Range, Reading \n";
+	for (int i = 0; (size_t)i < listofRanges.size(); i++)
+		outputText += std::to_string(listofRanges[i].lowerLimit) + "-" + std::to_string(listofRanges[i].upperLimit) + "," + std::to_string(listofRanges[i].numOfReadingsInRange) + "\n";
+	return outputText;
+}
+
+int validateReadings(std::vector<double> readingsList, int sensorType)
+{
+	if (readingsList.empty())
+	{
+		return 0;
+	}
+	for (std::vector<double>::iterator itr = readingsList.begin(); itr != readingsList.end(); itr++)
+	{
+		if (*itr < 0 || *itr > MaximumLimitsMap[sensorType])
+			return 0;
+	}
+	return 1;
+}
+
+std::vector<double> convertToAmps(std::vector<double> readingsList, int sensorType)
+{
+	funcptr coverterFunction = ConverterFunctionMap[sensorType];
+	std::vector<double> outputList = (*coverterFunction)(readingsList);
+	return outputList;
+}
+
+std::vector<double> converterfor12BitSensor(std::vector<double> readingsList)
+{
+	std::vector<double> convertedReadingsList = {};
+	for (std::vector<double>::iterator itr = readingsList.begin(); itr != readingsList.end(); itr++)
+	{
+		double valueInAmp = ((10 * (*itr)) / 4094);
+		convertedReadingsList.push_back(round(valueInAmp));
+	}
+	return convertedReadingsList;
+}
+
+std::vector<double> converterfor10BitSensor(std::vector<double> readingsList)
+{
+	std::vector<double> convertedReadingsList = {};
+	for (std::vector<double>::iterator itr = readingsList.begin(); itr != readingsList.end(); itr++)
+	{
+		double valueInAmp = (((*itr) - 511.0) * 15.0 / 511.0);
+		convertedReadingsList.push_back(abs(round(valueInAmp)));
+	}
+	return convertedReadingsList;
+}
+
+std::vector<Range> findRanges(std::vector<double> readingsList)
 {
 	std::vector<Range> listofRanges = {};
 	Range currentRange = {};
@@ -18,7 +79,7 @@ std::vector<Range> findRanges(std::vector<int> readingsList)
 	currentRange.lowerLimit = readingsList[0];
 	currentRange.upperLimit = readingsList[0];
 	currentRange.numOfReadingsInRange = 0;
-	for (std::vector<int>::iterator itr = readingsList.begin(); itr != readingsList.end(); itr++)
+	for (std::vector<double>::iterator itr = readingsList.begin(); itr != readingsList.end(); itr++)
 	{
 		if (*itr - currentRange.upperLimit <= 1)
 		{
@@ -37,7 +98,7 @@ std::vector<Range> findRanges(std::vector<int> readingsList)
 	return listofRanges;
 }
 
-std::vector<Range> computeCurrentRanges(std::vector<int> readingsList)
+std::vector<Range> computeCurrentRanges(std::vector<double> readingsList)
 {
 	std::vector<Range> listofRanges = {};
 	
@@ -45,8 +106,22 @@ std::vector<Range> computeCurrentRanges(std::vector<int> readingsList)
 	{
 		return {};
 	}
-	
+
 	listofRanges = findRanges(readingsList);
-	displayRanges(listofRanges);
+	std::string outputText = formatOutputToCsv(listofRanges);
+	printOnConsole(outputText);
 	return listofRanges;
+}
+
+std::vector<Range> computeCurrentRangesFromADCReading(std::vector<double> readingsList , int sensorType)
+{
+	int validReadings = validateReadings(readingsList, sensorType);
+
+	if (validReadings)
+	{
+		std::vector<double> convertedReadingsList = convertToAmps(readingsList, sensorType);
+		std::vector<Range> listofRanges = computeCurrentRanges(convertedReadingsList);
+		return listofRanges;
+	}
+	return{};
 }
